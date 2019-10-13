6699880d3dc4211da592d1467eab33eb6efcefe4
Shama Hoque <shama.hoque@gmail.com>
2019-10-13 13:29:30 +0600

    cw6

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Offline Reading List Storage
+
+Using the browser's localstorage store your reading list, where 
+* books can be added using a form, and 
+* stored books viewed in list  
---
+++ script.js
@@ -0,0 +1,19 @@
+
+/* Check localStorage for books */
+
+/* Check if books is empty and show/hide relevant message */ 
+
+/* If books is not emply, populate view with book details in #list */
+
+/* Add a book button click should reveal the form */
+
+/* On ADD button click, 
+    ensure the input fields contain values, 
+    then add the book to HTML view 
+    also save updated books array to localStorage 
+*/
+
+/* Refreshing the page, should show all the stored books */
+
+
+let books = [] // array containing book objects {title: '', author: ''}
